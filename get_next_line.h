c3a8d5212d33f63795bae9fb60be7af315255938
Achille Charvoz <acharvoz@c1r4p3.42nice.fr>
2024-06-18 16:40:25 +0200

    get_next_line.c

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acharvoz <acharvoz@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/18 14:21:04 by acharvoz          #+#    #+#             */
+/*   Updated: 2024/06/18 14:21:30 by acharvoz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+
+#endif
